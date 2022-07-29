2fe42c6f018814906b0a7ecc13736ef9bd8708f7
OlivierORRataj <olivier.rataj@edu.newton.se>
2022-07-29 15:06:20 +0200

    Implement basic Interface for comments

---
+++ SharpTrigger/ITrigger.cs
@@ -0,0 +1,26 @@
+﻿namespace SharpTrigger
+{
+    /// <summary>
+    /// Interface containing comments
+    /// </summary>
+    internal interface ITrigger
+    {
+        /// <summary>
+        /// Whether this <see cref="ITrigger"/> instance has an assigned Delegate
+        /// </summary>
+        bool HasDelegate { get; }
+        /// <summary>
+        /// Whether this <see cref="ITrigger"/> instance has fired
+        /// </summary>
+        bool HasFired { get; }
+
+        /// <summary>
+        /// Fires the <see cref="ITrigger"/> if <see cref="HasFired"/> is <see langword="false"/>
+        /// </summary>
+        void Fire();
+        /// <summary>
+        /// Reloads the <see cref="ITrigger"/> instance to re-enable <see cref="ITrigger.Fire"/>
+        /// </summary>
+        void Reload();
+    }
+}

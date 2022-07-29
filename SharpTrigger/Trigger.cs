306fd738c24779cdd159fd5115c033e5f95d006a
OlivierORRataj <olivier.rataj@edu.newton.se>
2022-07-29 15:13:08 +0200

    Trigger: Add generics

--- SharpTrigger/Trigger.cs
+++ SharpTrigger/Trigger.cs
@@ -54,4 +54,169 @@
             hasFired = false;
         }
     }
+
+    public class Trigger<T1> : Trigger
+    {
+        Action<T1> trigger;
+
+        public virtual bool HasDelegate { get { return trigger != null && trigger.Method != null; } }
+
+        protected static Trigger<T1> Init(Action<T1> method)
+        {
+            return new Trigger<T1>() { trigger = method, hasFired = false };
+        }
+
+        public static implicit operator Trigger<T1>(Action<T1> a) => Init(a);
+        public static implicit operator Action<T1>(Trigger<T1> t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Trigger is null");
+            }
+            return t.trigger;
+        }
+
+
+        public virtual void Fire(T1 obj)
+        {
+            if (hasFired == false)
+            {
+                try
+                {
+                    trigger.Invoke(obj);
+                }
+                catch (Exception ex)
+                {
+                    if (HasDelegate == false) throw new InvalidOperationException("Trigger does not have Delegate");
+                }
+                finally
+                {
+                    hasFired = true;
+                }
+            }
+        }
+    }
+
+    public class Trigger<T1, T2> : Trigger
+    {
+        Action<T1, T2> trigger;
+
+        public virtual bool HasDelegate { get { return trigger != null && trigger.Method != null; } }
+
+        protected static Trigger<T1, T2> Init(Action<T1, T2> method)
+        {
+            return new Trigger<T1, T2>() { trigger = method, hasFired = false };
+        }
+
+        public static implicit operator Trigger<T1, T2>(Action<T1, T2> a) => Init(a);
+        public static implicit operator Action<T1, T2>(Trigger<T1, T2> t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Trigger is null");
+            }
+            return t.trigger;
+        }
+
+        public virtual void Fire(T1 obj, T2 obj2)
+        {
+            if (hasFired == false)
+            {
+                try
+                {
+                    trigger.Invoke(obj, obj2);
+                }
+                catch (Exception ex)
+                {
+                    if (HasDelegate == false) throw new InvalidOperationException("Trigger does not have Delegate");
+                }
+                finally
+                {
+                    hasFired = true;
+                }
+            }
+        }
+    }
+
+    public class Trigger<T1, T2, T3> : Trigger
+    {
+        Action<T1, T2, T3> trigger;
+
+        public virtual bool HasDelegate { get { return trigger != null && trigger.Method != null; } }
+
+        protected static Trigger<T1, T2, T3> Init(Action<T1, T2, T3> method)
+        {
+            return new Trigger<T1, T2, T3>() { trigger = method, hasFired = false };
+        }
+
+        public static implicit operator Trigger<T1, T2, T3>(Action<T1, T2, T3> a) => Init(a);
+        public static implicit operator Action<T1, T2, T3>(Trigger<T1, T2, T3> t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Trigger is null");
+            }
+            return t.trigger;
+        }
+
+        public virtual void Fire(T1 obj, T2 obj2, T3 obj3)
+        {
+            if (hasFired == false)
+            {
+                try
+                {
+                    trigger.Invoke(obj, obj2, obj3);
+                }
+                catch (Exception ex)
+                {
+                    if (HasDelegate == false) throw new InvalidOperationException("Trigger does not have Delegate");
+                }
+                finally
+                {
+                    hasFired = true;
+                }
+            }
+        }
+    }
+
+    public class Trigger<T1, T2, T3, T4> : Trigger
+    {
+        Action<T1, T2, T3, T4> trigger;
+
+        public virtual bool HasDelegate { get { return trigger != null && trigger.Method != null; } }
+
+        protected static Trigger<T1, T2, T3, T4> Init(Action<T1, T2, T3, T4> method)
+        {
+            return new Trigger<T1, T2, T3, T4>() { trigger = method, hasFired = false };
+        }
+
+        public static implicit operator Trigger<T1, T2, T3, T4>(Action<T1, T2, T3, T4> a) => Init(a);
+        public static implicit operator Action<T1, T2, T3, T4>(Trigger<T1, T2, T3, T4> t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Trigger is null");
+            }
+            return t.trigger;
+        }
+
+        public virtual void Fire(T1 obj, T2 obj2, T3 obj3, T4 obj4)
+        {
+            if (hasFired == false)
+            {
+                try
+                {
+                    trigger.Invoke(obj, obj2, obj3, obj4);
+                }
+                catch (Exception ex)
+                {
+                    if (HasDelegate == false) throw new InvalidOperationException("Trigger does not have Delegate");
+                }
+                finally
+                {
+                    hasFired = true;
+                }
+            }
+        }
+    }
 }
